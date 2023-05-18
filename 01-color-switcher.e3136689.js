1b950fdcbc80c411bf2553743f5aa94e40221fe7
SvetlanaBuivol <120977493+SvetlanaBuivol@users.noreply.github.com>
2023-05-18 13:19:48 +0000

    Deploying to gh-pages from @ SvetlanaBuivol/goit-js-hw-09@9b709467c288ef6a56ea8b3c50f052fea6c0f30f 🚀

---
+++ 01-color-switcher.e3136689.js
@@ -0,0 +1,2 @@
+const t=document.body,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");let n=null;e.addEventListener("click",(function(a){n=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.disabled=!0})),a.addEventListener("click",(function(t){clearInterval(n),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.e3136689.js.map
