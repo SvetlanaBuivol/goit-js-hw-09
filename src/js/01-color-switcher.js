9b709467c288ef6a56ea8b3c50f052fea6c0f30f
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-05-18 16:19:19 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.body;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerID = null;
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick(e) {
+        timerID = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000
+        );
+    startBtn.disabled = true;
+}
+
+function onStopClick(e) {
+    clearInterval(timerID);
+    startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
