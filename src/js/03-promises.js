5192f186ab708e09557a4b3ca7bae22b1874f01c
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-05-21 23:10:10 +0300

    +

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,4 @@
-import Notiflix, { Notify } from 'notiflix';
+import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
 
@@ -33,9 +33,9 @@ function createPromise(position, delay) {
 
 function getPromises(position, delay) {
   createPromise(position, delay).then(({ position, delay }) => {
-      Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
     }).catch(({ position, delay }) => {
-      Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
     }); 
 };
    
